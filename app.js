d31adc4743039a35978f6ac0bb6e71d4381ba1b2
Yyauw <yauyunier@gmail.com>
2025-01-03 13:51:27 -0500

    preparacion inicial del proyecto y readme actualizado

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 import express from "express";
+import usuarioRouter from "./routes/app/usuario";
 
 const app = express();
 const port = process.env.PORT || 3000;
+//MIDDLEWARES
+app.use(express.json()); //para aceptar peticiones formato json
+app.use(express.urlencoded({ extended: true })); //para aceptar peticiones con datos de formulario
 
+//ROUTER
+app.use("/usuario", usuarioRouter);// Toda las rutas de usuarioRouter van a ser accesibles desde /usuario
+
+//ROUTES
 app.get("/", (req, res) => {
     res.send("Bienvenido al api de tesis");
   });
