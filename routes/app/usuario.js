17ad9463a41e196a9c070676ab19ae048c6028fd
Yyauw <yauyunier@gmail.com>
2025-01-08 15:02:36 -0500

    Se agrego el paquete cors y endpoint de login para los usuarios creado

--- routes/app/usuario.js
+++ routes/app/usuario.js
@@ -6,13 +6,33 @@ const prisma = new PrismaClient(); //abre la conexion con la base de datos
 
 //GET USUARIO
 router.get("/:id", async (req, res) => {
-    const { id } = req.params;
-    const usuario = await prisma.usuario.findUnique({
-        where: {
-            id: Number(id),
-        },
-    });
+  const { id } = req.params;
+  const usuario = await prisma.usuario.findUnique({
+    where: {
+      id: Number(id),
+    },
+  });
   res.json(usuario);
+});
+
+//POST - LOGIN USUARIO
+router.post("/login", async (req, res) => {
+  const { usuario, clave } = req.body;
+  const claveEncriptada = clave; // Aquí deberías encriptar la clave antes de compararla
+  //NO SE ESTA ENCRIPTADO LA CLAVE POR AHORA
+  const datosUsuario = await prisma.usuario.findFirst({
+    where: {
+      usuario: usuario,
+      contrase_a: clave,
+    },
+  });
+  console.log(datosUsuario);
+  res.json(datosUsuario);
+});
+
+router.get("/", async (req, res) => {
+  const usuarios = await prisma.usuario.findMany();
+  res.json(usuarios);
 });
 
 export default router;
